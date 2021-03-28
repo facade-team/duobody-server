2fbcd95dcee71a0ec21c1c7a83316532ef1ed0bb
poki <ohsang1219@naver.com>
2021-03-28 16:17:35 +0900

    [etc]: cors 모둘 적용, bodyparser 모듈 삭제

--- loaders/expressLoader.js
+++ loaders/expressLoader.js
@@ -1,13 +1,15 @@
+import express from 'express'
 import cookieParser from 'cookie-parser'
-import bodyParser from 'body-parser'
+import cors from 'cors'
 import morgan from 'morgan'
 import api from '../api/routes'
 
 const expressLoader = (app) => {
   app.use(cookieParser())
-  app.use(bodyParser.json())
-  app.use(bodyParser.urlencoded({ extended: true }))
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: true }))
   app.use(morgan('dev'))
+  app.use(cors())
 
   app.use('/api', api)
 }
