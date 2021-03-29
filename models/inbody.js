61ba6fa22528d904f3036a0cd549a7c61443cfb5
poki <ohsang1219@naver.com>
2021-03-30 00:50:25 +0900

    [feat] : lesson 모델 수정 및 inbody 모델 추가

--- models/inbody.js
+++ models/inbody.js
@@ -5,7 +5,7 @@ const {
   Types: { ObjectId },
 } = Schema
 
-const lessonSchema = new Schema({
+const inbodySchema = new Schema({
   //trainer 참조
   trainerId: {
     type: ObjectId,
@@ -19,30 +19,30 @@ const lessonSchema = new Schema({
     required: true,
     ref: 'Trainee',
   },
+  //몸무게
+  weight: {
+    type: Number,
+  },
+
+  //bmi
+  bmi: {
+    type: Number,
+  },
+
+  //체지방
+  fat: {
+    type: Number,
+  },
+
+  //골격근량
+  skeletalMuscle: {
+    type: Number,
+  },
 
   //날짜
   date: {
     type: Date,
   },
-
-  //세션 참조 배열
-  sessions: [
-    {
-      type: ObjectId,
-      required: true,
-      ref: 'Session',
-    },
-  ],
-
-  //시작시간
-  start: {
-    type: Date,
-  },
-
-  //종료시간
-  end: {
-    type: Date,
-  },
 })
 
-export default mongoose.model('Lesson', lessonSchema)
+export default mongoose.model('Inbody', inbodySchema)
