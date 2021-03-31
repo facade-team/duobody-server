799c38be0a25509d866920d329938502279100f1
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-04-01 02:05:08 +0900

    [feat]: resUtil 함수 생성

---
+++ utils/resUtil.js
@@ -0,0 +1,20 @@
+// respond 를 보낼 때, 파라미터만 넘겨주면 respond 를 자동으로 만들어주는 유틸함수
+
+export default {
+  success: (res, statusCode, msg, data = null) => {
+    // data 는 defulat parameter 를 null 로 설정 (parameter 가 안 들어와도 됨)
+    return res.status(statusCode).json({
+      success: true,
+      statusCode,
+      msg,
+      data,
+    })
+  },
+  fail: (res, statusCode, msg) => {
+    return res.status(statusCode).json({
+      success: false,
+      statusCode,
+      msg,
+    })
+  },
+}
