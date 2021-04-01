f2a299a7c50a385fd7ee5882e7900c031e9ed546
poki <ohsang1219@naver.com>
2021-04-01 14:37:57 +0900

    [feat]: session

--- api/routes/session/index.js
+++ api/routes/session/index.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import Authenticate from '../middlewares/Authenticate'
-// import { getDate } from '../../../services/trainerService'
+// import { getDate } from '../../../services/sessionService'
 
 const router = express.Router()
+
+router.get('trainee/:traineeId', Authenticate, async (req, res, next) => {})
 
 export default router
