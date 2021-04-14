46e2de358f51caa4e064d062e563cb171c05f6d0
poki <ohsang1219@naver.com>
2021-04-14 15:06:54 +0900

    [wip]: logging

---
+++ src/config/winston/index.js
@@ -0,0 +1,43 @@
+import winston from 'winston'
+import winstonDaily from 'winston-daily-rotate-file'
+
+const logDir = 'logs'
+
+const { combine, timestamp, printf } = winston.format
+
+const logFormat = printf((info) => {
+  return `{$info.timestamp} ${info.level} ${info.message}`
+})
+
+const options = {
+  console: {},
+}
+
+const logger = winston.createLogger({
+  foramt: combine(
+    timestamp({
+      format: 'YYYY-MM-DD HH:MM:SS',
+    }),
+    logFormat
+  ),
+  transports: [
+    // info 레벨 로그를 저장할 파일 설정
+    new winstonDaily({
+      level: 'info',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir,
+      filename: `%DATE%.log`,
+      maxFiles: 30, // 30일치 로그 파일 저장
+      zippedArchive: true,
+    }),
+    // error 레벨 로그를 저장할 파일 설정
+    new winstonDaily({
+      level: 'error',
+      datePattern: 'YYYY-MM-DD',
+      dirname: logDir + '/error', // error.log 파일은 /logs/error 하위에 저장
+      filename: `%DATE%.error.log`,
+      maxFiles: 30,
+      zippedArchive: true,
+    }),
+  ],
+})
