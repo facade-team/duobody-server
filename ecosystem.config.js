c3b0c8791953903c6e35db1ec29ece51349dea48
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-04-18 21:19:40 +0900

    [etc]: pm2 cluster mode 로 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
       watch: true,
       watch_delay: 1000,
       ignore_watch: ['node_modules', 'logs/', 'dist/'],
-      // instances: 0,
-      // exec_mode: 'cluster',
+      instances: 0,
+      exec_mode: 'cluster',
       env: {
         DEVELOPMENT: true,
       },
