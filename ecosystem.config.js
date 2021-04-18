375da1a0ee26b87ef30c73d0d0b7e95bb0b59e2f
Keep-Going-HyeonSoo <chs98105@gmail.com>
2021-04-18 21:04:10 +0900

    [feat]: pm2 watch 옵션 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,10 +2,12 @@ module.exports = {
   apps: [
     {
       name: 'duobody-server',
-      script: './dist/app.js',
-      watch: false,
-      instances: 0,
-      exec_mode: 'cluster',
+      script: 'dist/app.js',
+      watch: true,
+      watch_delay: 1000,
+      ignore_watch: ['node_modules', 'logs/', 'dist/'],
+      // instances: 0,
+      // exec_mode: 'cluster',
       env: {
         DEVELOPMENT: true,
       },
